0915f712fffdd3fdc75e673fe58b180c96e9ce4f
mzalbudukhi <soso_502@hotmail.com>
2015-04-24 15:28:14 -0500

    Update HeapSkew.h

--- HeapSkew.h
+++ HeapSkew.h
@@ -6,124 +6,123 @@
 template < class T >
 class HeapSkew : public Drawable
 {
-
-   private:
-      BinaryTree<T>* bt;
-      int sze;
-      BinaryTree<T>* merge(BinaryTree<T>* left, BinaryTree<T>* right);
-
-      int (*compare_items) (T* item_1, T* item_2);
-
-   public:
-      HeapSkew(int (*comp_items) (T* item_1, T* item_2));
-      virtual ~HeapSkew();
-
-      bool heapIsEmpty();
-      void heapInsert(T* item);
-      T* heapRemove();
-
-      void draw(Cairo::RefPtr<Cairo::Context> cr, int width, int height);
-      void mouseClicked(int x, int y);
-
+    
+private:
+    BinaryTree<T>* bt;
+    int sze;
+    BinaryTree<T>* merge(BinaryTree<T>* left, BinaryTree<T>* right);
+    
+    int (*compare_items) (T* item_1, T* item_2);
+    
+public:
+    HeapSkew(int (*comp_items) (T* item_1, T* item_2));
+    virtual ~HeapSkew();
+    
+    bool heapIsEmpty();
+    void heapInsert(T* item);
+    T* heapRemove();
+    
+    void draw(Cairo::RefPtr<Cairo::Context> cr, int width, int height);
+    void mouseClicked(int x, int y);
+    
 };
 
 // this is a MAXHEAP (largest items to top alphabetical order)
 template < class T >
 HeapSkew<T>::HeapSkew(int (*comp_items) (T* item_1, T* item_2)) : Drawable()
-{ 
-   bt = new BinaryTree<T>();
-   sze = 0;
-
-   compare_items = comp_items;
+{
+    bt = new BinaryTree<T>();
+    sze = 0;
+    
+    compare_items = comp_items;
 }
 
 template < class T >
-HeapSkew<T>::~HeapSkew() 
-{ 
-   delete bt;
-}  
+HeapSkew<T>::~HeapSkew()
+{
+    delete bt;
+}
 
 template < class T >
 bool HeapSkew<T>::heapIsEmpty()
 {
-   return sze == 0;
+    return sze == 0;
 }
 
 template < class T >
 BinaryTree<T>* HeapSkew<T>::merge(BinaryTree<T>* left, BinaryTree<T>* right)
 {
-   //DO THIS
-
-	if(left->isEmpty()) 
-	{
-		delete left;
-		return right;
-	}
-	if(right->isEmpty())
-	{
-		delete right;
-		return left;
-	}
-	
-	int comp = (compare_items) (left->getRootItem(), right->getRootItem());
-	if(comp < 0) return merge(right, left);
-	
-	BinaryTree<T>* ll = left->detachLeftSubtree();
-	BinaryTree<T>* lr = left->detachRightSubtree();
-	
-	left->attachRightSubtree(ll);
-	delete ll;
-	
-	if(lr->isEmpty()) 
-	{
-		left->attachLeftSubtree(right);
-		delete right;
-		delete lr;
-		return left;
-	}
-	else
-	{
-		BinaryTree<T>* new_tree = merge(lr, right);
-		left->attachLeftSubtree(new_tree);
-		delete new_tree;
-		return left;
-	}
-	
-	//return left;
-
-
+    //DO THIS
+    if (left->isEmpty())
+    {
+        delete left;
+        return right;
+    }
+    else if (right->isEmpty())
+    {
+        delete right;
+        return left;
+    }
+    
+    int compare = (*compare_items) (left->getRootItem(), right->getRootItem());
+    
+    if (compare < 0)
+    {
+        return merge(right, left);
+    }
+    
+    BinaryTree<T>* LL = left->detachLeftSubtree();
+    BinaryTree<T>* LR = left->detachRightSubtree();
+    left->attachRightSubtree(LL);
+    delete LL;
+    if (LR->isEmpty())
+    {
+        delete LR;
+        left->attachLeftSubtree(right);
+        delete right;
+        return left;
+    }
+    
+    BinaryTree<T>* blah=merge(LR, right);
+    left->attachLeftSubtree(blah);
+    
+    //delete LR;
+    delete blah;
+    return left;
+    
 }
 
 template < class T >
 void HeapSkew<T>::heapInsert(T* item)
 {
-   //DO THIS (calls merge, should be short)
-	//BinaryTree<T>* left = bt;
-	BinaryTree<T>* right = new BinaryTree<T>(item);
-	bt = merge(bt,right);
-
-   sze++;
+    //DO THIS (calls merge, should be short)
+    BinaryTree<T>* tree = new BinaryTree<T>(item);
+    bt=merge(bt, tree);
+    
+    
+    
+    
+    sze++;
 }
 
 template < class T >
 T* HeapSkew<T>::heapRemove()
 {
-   //DO THIS (calls merge, should be short)
-   T* result = bt->getRootItem();
-   BinaryTree<T>* root = bt;
-	BinaryTree<T>* left = bt->detachLeftSubtree();
-	BinaryTree<T>* right = bt->detachRightSubtree();
-	bt = merge(left,right);
-
-	delete root;
-   sze--;
-   return result;
+    //DO THIS (calls merge, should be short)
+    BinaryTree<T>* left = bt->detachLeftSubtree();
+    BinaryTree<T>* right = bt->detachRightSubtree();
+    T* result = bt->getRootItem();
+    delete bt;
+    bt=merge(left, right);
+    
+    sze--;
+    return result;
 }
 
 template < class T >
 void HeapSkew<T>::draw(Cairo::RefPtr<Cairo::Context> cr, int width, int height)
 {
-   bt->draw(cr, width, height);
+    bt->draw(cr, width, height);
 }
 
 template < class T >
